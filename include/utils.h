9b352389d5290328a1019f5da483ba8904c4e830
David Hedin <lucky.hedin13@gmail.com>
2014-04-14 22:44:39 -0400

    fixed kernel test

--- include/utils.h
+++ include/utils.h
@@ -2,12 +2,14 @@
 #ifndef __UTILS_H_INCLUDED__
 #define __UTILS_H_INCLUDED__
 
-void max_func(short *data, short *temp, short *out);
+__device__ void max_func(short *data, short *temp, short *out);
 
-void min_func(short *data, short *temp, short *out);
+__device__ void min_func(short *data, short *temp, short *out);
 
-void block_scan(int *data);
+__device__ void block_scan(int *data);
 
-void radix_sort(int *data, int *temp1, int *temp2);
+__device__ void radix_sort(int *data, int *temp1, int *temp2);
+
+__global__ void test_kernel(int *test_int_data, short *test_short_data);
 
 #endif
